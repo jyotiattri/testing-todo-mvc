cf5c521ac932d7c1d533e5763c84c938ff84723f
jyoti attri <jyotiatri22@gmail.com>
2020-06-01 13:12:25 +0530

    day 27 changes made

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,21 +1,21 @@
-import Vue from 'vue'; 
+import Vue from 'vue';
 import App from '@/App';
-import { shallowMount } from '@vue/test-utils';
 
-
-describe('App', () => {
-  it ('should render correct contents', () => {
-    let wrapper = shallowMount(App);
-    expect(wrapper.find('.title').text())
-    .toBe('todos');
-    expect(wrapper.find('.new-todo').element.placeholder)
-    .toBe('What needs to be done?');
+describe('App.vue', () => {
+  it('should render correct contents', () => {
+    const Constructor = Vue.extend(App);
+    const vm = new Constructor().$mount();
+    
+    expect(vm.$el.querySelector('.title').textContent)
+      .toBe('todos');
+    expect(vm.$el.querySelector('.new-todo').placeholder)
+      .toBe('What needs to be done?');
   });
-
+  
   it('should set correct default data', () => {
     const initialData = App.data();
 
     expect(initialData.todos).toEqual([]);
-    expect(initialData.newTodos).toEqual('');
+    expect(initialData.newTodo).toEqual('');
   });
 });
